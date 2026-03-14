d9fc5647ced4937e63a22056464d311a49626e36
sneha jadeja <snehajadeja011@gmail.com>
2026-03-14 14:33:46 +0530

    changes in otp

--- server.js
+++ server.js
@@ -1,86 +1,34 @@
 const express = require("express");
 const path = require("path");
-const User = require("./models/User");
-const otpGenerator = require("otp-generator");
+const mongoose = require("mongoose");
 const nodemailer = require("nodemailer");
-
-require("./db");
+const otpGenerator = require("otp-generator");
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname,"public")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "login.html"));
+
+// MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/waretrack")
+.then(()=>console.log("MongoDB Connected"))
+.catch(err=>console.log(err));
+
+
+// User schema
+const userSchema = new mongoose.Schema({
+email:String,
+password:String,
+otp:String
 });
 
-
-// SIGNUP
-app.post("/signup", async (req, res) => {
-
-  const { email, password } = req.body;
-
-  const existingUser = await User.findOne({ email });
-
-  if (existingUser) {
-    return res.send("User already exists");
-  }
-
-  const user = new User({
-    email,
-    password,
-    otp: ""
-  });
-
-  await user.save();
-
-  res.send("Signup successful. Please login.");
-});
+const User = mongoose.model("User",userSchema);
 
 
-// LOGIN
-app.post("/login", async (req, res) => {
-
-  const { email, password } = req.body;
-
-  const user = await User.findOne({ email, password });
-
-  if (!user) {
-    return res.send("Invalid credentials");
-  }
-
-  res.redirect("/dashboard.html");
-});
-
-
-// SEND OTP
-app.post("/send-otp", async (req, res) => {
-
-  const { email } = req.body;
-
-  const user = await User.findOne({ email });
-
-  if (!user) {
-    return res.send("User not found");
-  }
-
-  const otp = otpGenerator.generate(6, {
-    digits: true,
-    alphabets: false,
-    upperCaseAlphabets: false,
-    specialChars: false
-  });
-
-  user.otp = otp;
-  await user.save();
-
-  console.log("Generated OTP:", otp);
-
-  // email transporter
-  const transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
       user: "snehajadejatest01@gmail.com",
@@ -88,49 +36,109 @@ app.post("/send-otp", async (req, res) => {
     }
   });
 
-  await transporter.sendMail({
-    from: "snehajadejatest01@gmail.com",
-    to: email,
-    subject: "Password Reset OTP",
-    text: `Your OTP for password reset is: ${otp}`
-  });
 
-  res.send("OTP sent to your email");
+// Home
+app.get("/",(req,res)=>{
+res.sendFile(path.join(__dirname,"public","login.html"));
 });
 
 
-// VERIFY OTP
-app.post("/verify-otp", async (req, res) => {
+// Signup
+app.post("/signup",async(req,res)=>{
 
-  const { email, otp } = req.body;
+const {email,password}=req.body;
 
-  const user = await User.findOne({ email, otp });
+const user=new User({email,password});
 
-  if (!user) {
-    return res.send("Invalid OTP");
-  }
+await user.save();
 
-  res.redirect("/reset.html");
+res.send("Signup successful");
+
 });
 
 
-// RESET PASSWORD
-app.post("/reset-password", async (req, res) => {
+// Login
+app.post("/login",async(req,res)=>{
 
-  const { email, password } = req.body;
+const {email,password}=req.body;
 
-  await User.updateOne(
-    { email },
-    {
-      password: password,
-      otp: ""
-    }
-  );
+const user=await User.findOne({email,password});
 
-  res.send("Password reset successful");
+if(!user){
+return res.send("Invalid credentials");
+}
+
+res.redirect("/dashboard.html");
+
 });
 
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+// Send OTP
+app.post("/send-otp",async(req,res)=>{
+
+const {email}=req.body;
+
+const user=await User.findOne({email});
+
+if(!user){
+return res.send("User not found");
+}
+
+const otp=otpGenerator.generate(6,{
+digits:true,
+alphabets:false,
+upperCase:false,
+specialChars:false
+});
+
+user.otp=otp;
+await user.save();
+
+console.log("OTP:",otp);
+
+await transporter.sendMail({
+from:"YOUR_EMAIL@gmail.com",
+to:email,
+subject:"Password Reset OTP",
+text:`Your OTP is ${otp}`
+});
+
+res.redirect("/otp.html");
+
+});
+
+
+// Verify OTP
+app.post("/verify-otp",async(req,res)=>{
+
+const {email,otp}=req.body;
+
+const user=await User.findOne({email,otp});
+
+if(!user){
+return res.send("Invalid OTP");
+}
+
+res.redirect("/reset.html");
+
+});
+
+
+// Reset password
+app.post("/reset-password",async(req,res)=>{
+
+const {email,password}=req.body;
+
+await User.updateOne(
+{email},
+{password,otp:""}
+);
+
+res.send("Password reset successful");
+
+});
+
+
+app.listen(3000,()=>{
+console.log("Server running on http://localhost:3000");
 });
