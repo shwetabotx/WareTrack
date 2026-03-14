07a768468fb895492b878460205390db1512540d
shwetabotx <s.j.jadeja69@gmail.com>
2026-03-14 09:26:29 +0530

    products logic

---
+++ public/script.js
@@ -0,0 +1,37 @@
+async function addProduct(){
+
+const name=document.getElementById("name").value
+const sku=document.getElementById("sku").value
+const category=document.getElementById("category").value
+const stock=document.getElementById("stock").value
+const location=document.getElementById("location").value
+
+await fetch("/products",{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({name,sku,category,stock,location})
+})
+
+loadProducts()
+
+}
+
+async function loadProducts(){
+
+const res=await fetch("/products")
+const products=await res.json()
+
+const list=document.getElementById("productList")
+list.innerHTML=""
+
+products.forEach(p=>{
+const li=document.createElement("li")
+li.textContent=`${p.name} | Stock:${p.stock} | ${p.location}`
+list.appendChild(li)
+})
+
+}
+
+loadProducts()
